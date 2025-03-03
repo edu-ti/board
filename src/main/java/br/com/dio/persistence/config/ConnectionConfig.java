6b03cedfa20edc1fc47dd3ad89ada9e11e21affd
edu-ti <educs85.ti@gmail.com>
2025-03-03 17:03:14 -0300

    Initial commit

---
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -0,0 +1,21 @@
+package br.com.dio.persistence.config;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ConnectionConfig {
+
+    public static java.sql.Connection getConnection() throws SQLException {
+        var url = "jdbc:mysql://localhost:3306/board";
+        var user = "board";
+        var password = "board";
+        var connection = DriverManager.getConnection(url, user, password);
+        connection.setAutoCommit(false);
+        return connection;
+    }
+}
